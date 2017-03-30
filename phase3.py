97b0822eaf906089bd822a283690b92d78887f75
Fady Machaalani <fmachaalani@gmail.com>
2017-03-29 22:46:53 -0600

    Working Phase3 - except for one part marked TODO

--- phase3.py
+++ phase3.py
@@ -1,10 +1,12 @@
 from bsddb3 import db
 import re 
+import sys
 import time
+import os
 
 def main():
 	RUNNING = True
-
+		
 	while RUNNING:
 		choice = input("Enter query or exit: ").strip().lower()
 		if(choice.strip() == "exit"):
@@ -12,6 +14,7 @@ def main():
 			break
 		else:
 			getQueries(choice)
+			break
 
 def getQueries(choice):
 	queries = choice.split()
@@ -27,7 +30,6 @@ def getQueries(choice):
 def parseDate(date):
 	#Iterates through the index file for the dates. If it finds
 	# the date that matches, it will getTweet using the TweetID
-	print("Parsing Date")
 	modifer = None
 	validDate = re.findall("[0-9]{4}/[0|1]{1}[0-9]{1}/[0-3]{1}[0-9]{1}", date)
 	newDate = time.strptime(validDate[0], "%Y/%m/%d")
@@ -38,55 +40,146 @@ def parseDate(date):
 	dateDatabase.open("da.idx")
 	dateCursor = dateDatabase.cursor()
 	iterator = dateCursor.first()
+	tweetList = []
 	while iterator:
 		tweetDate = iterator[0]
 		tweetID = iterator[1]
 		tempDate = time.strptime(tweetDate.decode("utf-8"), "%Y/%m/%d")
 		if(modifer == ":" or modifer == None):
 			if(newDate == tempDate):
-				getTweet(tweetID.decode("utf-8"))
+				tweetList.append(getTweet(tweetID.decode("utf-8")))
 		elif(modifer == ">"):
 			#This returns all dates larger than the entered date
 			if(newDate < tempDate):
-				getTweet(tweetID.decode("utf-8"))
+				tweetList.append(getTweet(tweetID.decode("utf-8")))
 		elif(modifer == "<"):
 			#This returns all dates smaller than the entered date
 			if(newDate > tempDate):
-				getTweet(tweetID.decode("utf-8"))
+				tweetList.append(getTweet(tweetID.decode("utf-8")))
 		iterator = dateCursor.next()
 	dateCursor.close()
 	dateDatabase.close()
+	formatData(tweetList)
 
-
-def parseTerm(term):
-	print("Parsing Term")
-
-
+#Retrieve the tweet with the matching ID
 def getTweet(tweetID):
 	tweetDatabase = db.DB()
 	tweetDatabase.open("tw.idx")
 	tweetCursor = tweetDatabase.cursor()
-	test = tweetID.encode("utf-8")
-	tempData = tweetDatabase.get(test).decode("utf-8")	
-	print(tempData)
-	# formatData(tempData)
-
+	encodedID = tweetID.encode("utf-8")
+	tempData = tweetDatabase.get(encodedID).decode("utf-8")	
 	tweetDatabase.close()
 	tweetCursor.close()
+	return tempData
 
-# def formatData(data):
-# 	print("ID:" + recordID)
-# 	print("Date:" + recordDate)
-# 	print("Text:" + recordText)
-# 	print("Retweet Count:" count)
-# 	print("Name:" + name)
-# 	print("Location:" + location)
-# 	print("Description:" + desc)
-# 	print("URL:" + url)
+#The option menu is manipulated here
+def formatData(tweetList):
+	amount = len(tweetList)
+	counter = 0
+	os.system("clear")
+	if(amount == 0):
+		FLAG = False
+	else:
+		FLAG = True
+		dataPrinter(tweetList[counter])
+	print("\n %d Option(s)" %amount)
+	if(amount > 1):
+		command = input("n To see Another or exit: ").strip().lower()
+		if(command == "exit"):
+			FLAG = False
+		while (FLAG):
+			if(command == 'n'):
+				counter += 1
+				if(counter >= amount):
+					print("No more to see")
+					break
+				else:
+					os.system("clear")
+					print("%d/%d" %(counter+1, amount))
+					dataPrinter(tweetList[counter])
+					command = input("Press n To See Another: ").strip().lower()
+			elif(command == "exit"):
+				FLAG = False
+				break
+			else:
+				command = input("Enter a Valid Option: ").strip().lower()
+				
+
+
+def parseTerm(term):	
+	nameQuery = None
+	textQuery = None
+	locationQuery = None
+	query = None
+
+	if(":" in term):
+		text = term.split(":")
+		if(text[0] ==  "name"):
+			query = "n-" + text[1]
+		elif(text[0] == "text"):
+			query = "t-" + text[1]
+		elif(text[0] == "location"):
+			query = "l-" + text[1]
+	elif("%" in term):
+		pass 
+		#TODO Need to do the partial search here
+		
+	else:
+		nameQuery = "n-" + term
+		textQuery = "t-" + term
+		locationQuery = "l-" + term
+
+	if(nameQuery != None):
+		runQuery(nameQuery)
+	if(textQuery != None):
+		runQuery(textQuery)
+	if(locationQuery != None):
+		runQuery(locationQuery)
+	if(query != None):
+		runQuery(query)
+	
+
+
+def runQuery(query):
+	termDatabase = db.DB()
+	termDatabase.open("te.idx")
+	termCursor = termDatabase.cursor()
+	iterator = termCursor.first()
+
+	tweetList = []
+	while iterator:
+		if(iterator[0].decode("utf-8") == query):
+			tweetID = iterator[1].decode("utf-8")
+			tempTweet= getTweet(tweetID)
+			if(tempTweet not in tweetList):
+				tweetList.append(tempTweet)
+		iterator = termCursor.next()	
+	formatData(tweetList)
+
+
+#Prints the data in a human-readable format 
+def dataPrinter(data):
+	recordID = re.findall("<id>(.*?)<\/id>", data)[0]
+	recordDate = re.findall("<created_at>(.*?)<\/created_at>", data)[0]
+	recordText = re.findall("<text>(.*?)<\/text>", data)[0]
+	count = re.findall("<retweet_count>(.*?)<\/retweet_count>", data)[0]
+	name = re.findall("<name>(.*?)<\/name>", data)[0]
+	location = re.findall("<location>(.*?)<\/location>", data)[0]
+	desc = re.findall("<description>(.*?)<\/description>", data)[0]
+	url = re.findall("<url>(.*?)<\/url>", data)[0]
+	print("\n")
+	print("ID: " + recordID)
+	print("Date: " + recordDate)
+	print("Text: " + recordText)
+	print("Retweet Count: " + count)
+	print("Name: " + name)
+	print("Location: " + location)
+	print("Description: " + desc)
+	print("URL: " + url)
+	print("\n")
 
 
 
 
-# getTweet("000000018")
+
 main()
-
